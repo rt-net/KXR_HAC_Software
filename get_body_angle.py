f2c922177a4122e72964a1ba7e5bc7412b70b1ce
TaikiTsuno <taikiyoyo@gmail.com>
2023-03-29 15:51:09 +0900

    Revert "横移動の自己位置推定を追加"
    This reverts commit 4efc8e28b2d3665ee7e7495801eec16795033fc1.

--- get_body_angle.py
+++ get_body_angle.py
@@ -4,7 +4,7 @@ import time
 i2c = board.I2C()
 
 sensor = adafruit_bno055.BNO055_I2C(i2c) #センサー値を読み込む
-time.sleep(1.0)
+#time.sleep(1.0)
 yawinit = sensor.euler[0] #yaw初期角度設定(毎回電源投入時にキャリブレーションされるので今は使ってない)
 pitchinit = sensor.euler[1] #pitch初期角度設定(毎回電源投入時にキャリブレーションされるので今は使ってない)
 rollinit = sensor.euler[2] #roll初期角度設定(毎回電源投入時にキャリブレーションされるので今は使ってない)
--- walk_sideway.py
+++ walk_sideway.py
@@ -1,9 +1,6 @@
 import sys
 sys.path.append('../Rcb4Lib') #Rcb4Libの検索パスを追加
 from Rcb4BaseLib import Rcb4BaseLib            #Rcb4BaseLib.pyの中のRcb4BaseLibが使えるように設定
-import global_value as g
-import get_body_angle
-import math
 
 rcb4 = Rcb4BaseLib()      #rcb4をインスタンス(定義)
         
@@ -11,33 +8,27 @@ rcb4 = Rcb4BaseLib()      #rcb4をインスタンス(定義)
 rcb4.open('/dev/ttyUSB0',115200,1.3)
 
 g.walkStepDist = 16 #歩行動作1回あたりの移動量(cm)
-walkStepDist = 16
+walkStepDist = g.walkStepDist
 g.X = 0
 g.Y = 0
 
 def main(walkDist): #メイン関数　引数は移動量
     i = 0 #繰り返しに使うiの初期値設定
-    BodyAngle = get_body_angle.main() #IMUの価を読み込む
-    #print(BodyAngle) #自己角度を表示
-
     if rcb4.checkAcknowledge() == True:  #通信が返ってきたとき
         #print ('MotionPlay(1)')
         stepCount = (round(walkDist/walkStepDist)) #歩行動作を行う回数を確定
-        print(stepCount) #歩行動作繰り返し回数を表示
+        print(stepCount)
         if walkDist < 0: #移動量が負の時(左へ移動のとき)
-            
             stepCount = abs(stepCount)
             while i < stepCount: #定められた歩行回数まで繰り返し
-                BodyAngle = get_body_angle.main() #自己姿勢を読み込む
+                BodyAngle = get_body_angle.main()
                 rcb4.motionPlay(3)       #モーション番号3(左横移動)を再生
-                
                 if BodyAngle[0] > 0: #Yaw軸周りの角度が正の時
                     g.X = g.X + math.cos(math.radians(BodyAngle[0]))*(-walkStepDist) #自己位置に[cos(θ)*一歩の移動量 = 負]を加算
                     g.Y = g.Y + math.sin(math.radians(BodyAngle[0]))*(walkStepDist) #自己位置に[sin(θ)*一歩の移動量]を加算
                 else: #Yaw軸周りの角度が負の時
                     g.X = g.X + math.cos(math.radians(BodyAngle[0]))*(-walkStepDist) #自己位置に[cos(θ)*一歩の移動量 = 負]を加算
                     g.Y = g.Y + math.sin(math.radians(BodyAngle[0]))*(walkStepDist) #自己位置に[sin(θ)*一歩の移動量 = 負]を加算
-                
                 while True:     #モーションの再生が終わるまで繰り返し
                     motionNum = rcb4.getMotionPlayNum()   #現在再生されているモーション番号を取得
                     if motionNum < 0:                     #モーション番号が0より小さい場合はエラー
@@ -45,21 +36,18 @@ def main(walkDist): #メイン関数　引数は移動量
                     if motionNum == 0:                    #モーション番号が0のときは再生されていない状態
                         break
                 i = i+1
-            
                 
         else: #移動量が正の時(右へ移動の時)
-            
             while i < stepCount: #定められた歩行回数まで繰り返し
                 BodyAngle = get_body_angle.main() #自己姿勢を読み込む
                 rcb4.motionPlay(4)     #モーション番号4(右横移動)を再生
-                
+                BodyAngle = get_body_angle.main()
                 if BodyAngle[0] > 0: #Yaw軸周りの角度が正の時
                     g.X = g.X + math.cos(math.radians(BodyAngle[0]))*(walkStepDist) #自己位置に[cos(θ)*一歩の移動量]を加算
                     g.Y = g.Y + math.sin(math.radians(BodyAngle[0]))*(-walkStepDist) #自己位置に[sin(θ)*一歩の移動量 = 負]を加算
                 else: #Yaw軸周りの角度が負の時
                     g.X = g.X + math.cos(math.radians(BodyAngle[0]))*(walkStepDist) #自己位置に[cos(θ)*一歩の移動量]を加算
                     g.Y = g.Y + math.sin(math.radians(BodyAngle[0]))*(-walkStepDist) #自己位置に[sin(θ)*一歩の移動量]を加算
-                    
                 while True:     #モーションの再生が終わるまで繰り返し
                     motionNum = rcb4.getMotionPlayNum()   #現在再生されているモーション番号を取得
                     if motionNum < 0:                     #モーション番号が0より小さい場合はエラー
@@ -67,7 +55,7 @@ def main(walkDist): #メイン関数　引数は移動量
                     if motionNum == 0:                    #モーション番号が0のときは再生されていない状態
                         break
                 i = i+1
-                
+        
     
     else:  #通信が返ってきていないときはエラー
         print ('checkAcknowledge error')
