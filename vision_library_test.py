d737565e6e054ebbe49c67775a35e6c7a6cabe70
TaikiTsuno <taikiyoyo@gmail.com>
2023-05-24 13:10:49 +0900

    Revert "Revert "PEP8命名規則への修正""
    This reverts commit 996ce93df3a6d3f9bb60ec9e0e94d76c39be527c.

--- vision_library_test.py
+++ vision_library_test.py
@@ -10,13 +10,19 @@ while True:
     start = time.time()
     VISION.calibrate_img()
     linea, lineb = VISION.detect_edge_using_numpy_calc()
-    # ballx, bally = VISION.detect_ball()
-    # cornertype, cornerx, cornery = VISION.detect_corner()
-    # resultimg = VISION.display_resultimg()
-    # cv2.imshow("frame3", resultimg)
+    ballx, bally = VISION.detect_ball()
+    cornertype, cornerx, cornery = VISION.detect_corner()
+    
+    print("----------------------")
+    print(linea, lineb)
+    print(ballx, bally)
+    print(cornertype, cornerx, cornery)
+    
+    resultimg = VISION.display_resultimg()
+    cv2.imshow("frame3", resultimg)
     
     finish = time.time()
     
-    print(finish-start)
+    #print(finish-start)
     if cv2.waitKey(delay) & 0xFF == ord('q'):
         break
