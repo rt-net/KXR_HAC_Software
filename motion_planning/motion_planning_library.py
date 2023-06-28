95ff094a40fd7282f188d7fabf938740d914c461
TaikiTsuno <taikiyoyo@gmail.com>
2023-06-28 16:42:29 +0900

    primitivetaskの作りこみ

--- motion_planning/motion_planning_library.py
+++ motion_planning/motion_planning_library.py
@@ -8,21 +8,23 @@ from vision.vision_library import VisionLibrary
 from motion_control.motion_control_library import MotionLibrary
 from motion_planning import parameters
 
-BEV_FRAME_WIDTH = 345 #画角内に配置できる最大の長方形幅
-BEV_FRAME_HEIGHT = 395 #画角内に配置できる最大の長方形幅
+BEV_FRAME_WIDTH_MM = 345 #画角内に配置できる最大の長方形幅
+BEV_FRAME_HEIGHT_MM = 395 #画角内に配置できる最大の長方形幅
 
 class MotionPlanningLibrary:
     def __init__(self):
         self.VISION = VisionLibrary()
         self.MOTION = MotionLibrary()
         
-        self.distance_from_the_edge = parameters.WALK_PATH_FIELD_EDGE_DISTANCE
+        self.distance_from_the_edge_mm = parameters.WALK_PATH_TO_FIELD_EDGE_DEFAULT_MM
         
-        print("Motion Planning Initialize")
+        print("[行動計画の初期化成功]")
         
-    def get_distance_from_the_edge(self, slope, intercept):
-        Ax = parameters.BEV_FRAME_WIDTH/2
-        Ay = parameters.BEV_FRAME_HEIGHT + parameters.FOOT_CENTER_TO_BEV_FRAME_BOTTOM_DISTANCE
+    def calculate_distance_from_the_edge_mm(self, slope, intercept):
+        """Calculate current distance from robot-cog to edge
+        """
+        Ax = BEV_FRAME_WIDTH_MM/2
+        Ay = BEV_FRAME_HEIGHT_MM + parameters.FOOT_CENTER_TO_BEV_FRAME_BOTTOM_DISTANCE
         
         Bx = (Ay-intercept)/slope
         By = Ay
@@ -34,9 +36,11 @@ class MotionPlanningLibrary:
         AB = (Ax - Bx)
         BC = (AC**2 + AB**2)**(1/2)
         
-        self.distance_from_the_edge = (AB**2 - ((AB**2 - AC**2 +BC**2)/(2*BC))**2)**(1/2)
+        self.distance_from_the_edge_mm = (AB**2 - ((AB**2 - AC**2 +BC**2)/(2*BC))**2)**(1/2)
         
-    def get_vision(self):
+    def get_vision_all(self):
+        """collect all vision data
+        """
         self.VISION.calibrate_img()
         self.angle, self.slope, self.intercept = self.VISION.detect_edge_using_numpy_calc()
         self.ball_coordinate_x, self.ball_coordinate_y = self.VISION.detect_ball()
@@ -44,59 +48,71 @@ class MotionPlanningLibrary:
         self.result_image = self.VISION.display_resultimg()
         
     def left_hand_approach(self):
-        self.get_vision()
-        print(self.angle)
+        """execute a loop of left hand approach
+        """
+        self.get_vision_all()
+        #print(self.angle)
         
-        if self.MOTION.button_state() == False:
+        if self.MOTION.is_button_pressed == False:
             self.MOTION.walk_forward_continue()
             
         if self.angle != 0:
-            self.MOTION.motion_stop()
-            self.MOTION.turn(30)
-            #self.get_distance_from_the_edge(self, self.slope, self.intercept)
+            self.MOTION.stop_motion()
+            # self.MOTION.turn(self.angle)
+            self.calculate_distance_from_the_edge_mm(self.slope, self.intercept)
+            self.MOTION.turn(self.angle)
         
-        if self.distance_from_the_edge < parameters.EDGE_APPROACH_THRESHOLD:
-            self.MOTION.motion_stop()
-            self.MOTION.turn(angle)
-            if angle > 0:
-                self.MOTION.motion_stop()
-                self.MOTION.walk_sideway(-(dist-parameters.WALK_PATH_FIELD_EDGE_DISTANCE))
+        if self.distance_from_the_edge_mm < parameters.WALK_PATH_TO_FIELD_EDGE_MINIMUM_MM or self.distance_from_the_edge_mm > parameters.WALK_PATH_TO_FIELD_EDGE_MAXIMUM_MM:
+            self.MOTION.stop_motion()
+
+            if self.angle > 0:
+                self.MOTION.stop_motion()
+                self.MOTION.walk_sideway(-(self.distance_from_the_edge_mm-parameters.WALK_PATH_TO_FIELD_EDGE_DEFAULT_MM))
             else:
-                self.MOTION.motion_stop()
-                self.MOTION.walk_sideway((dist-parameters.WALK_PATH_FIELD_EDGE_DISTANCE))
+                self.MOTION.stop_motion()
+                self.MOTION.walk_sideway((self.distance_from_the_edge_mm-parameters.WALK_PATH_TO_FIELD_EDGE_DEFAULT_MM))
                 
-    def approach_to_ball(self):        
-        self.get_vision()
+        print("1")
+                
+    def approach_to_ball(self):
+        """execute full ball approach process
+        """
+        self.get_vision_all()
         
         if self.ball_coordinate_x != 0 and self.ball_coordinate_y != 0:
-            print(self.ball_coordinate_x, self.ball_coordinate_y)
+            #print(self.ball_coordinate_x, self.ball_coordinate_y)
             
-            robot_to_ball_distance_x = self.ball_coordinate_x-(BEV_FRAME_WIDTH/2)
-            robot_to_ball_distance_y = BEV_FRAME_HEIGHT - self.ball_coordinate_y
+            robot_to_ball_distance_x_mm = self.ball_coordinate_x-(BEV_FRAME_WIDTH_MM/2)
+            robot_to_ball_distance_y_mm = BEV_FRAME_HEIGHT_MM - self.ball_coordinate_y
             
-            print(robot_to_ball_distance_x)
+            #print(robot_to_ball_distance_x_mm)
             
-            robot_to_ball_angle = math.degrees(math.tan(robot_to_ball_distance_x/robot_to_ball_distance_y))
+            robot_to_ball_angle = math.degrees(math.tan(robot_to_ball_distance_x_mm/robot_to_ball_distance_y_mm))
             
-            print(robot_to_ball_angle)
-            
+            self.MOTION.stop_motion()
             self.MOTION.turn(robot_to_ball_angle)
             
             while True:
                 self.MOTION.walk_forward_continue()
-                self.get_vision()
-                robot_to_ball_distance_y = BEV_FRAME_HEIGHT - self.ball_coordinate_y
-                if robot_to_ball_distance_y < parameters.BALL_APPROACH_THRESHOLD:
+                self.get_vision_all()
+                robot_to_ball_distance_y_mm = BEV_FRAME_HEIGHT_MM - self.ball_coordinate_y
+                
+                if robot_to_ball_distance_y_mm < parameters.BALL_APPROACH_THRESHOLD:
                     self.MOTION.stop_motion()
-                    break
-            
-            robot_to_ball_distance_x = self.ball_coordinate_x-(BEV_FRAME_WIDTH/2)
-            
-            self.MOTION.walk_sideway(robot_to_ball_distance_x)
-            
-            self.MOTION.touch_ball()
-            
-            print("touched")
+                    robot_to_ball_distance_x_mm = self.ball_coordinate_x-(BEV_FRAME_WIDTH_MM/2)
+                    self.MOTION.walk_sideway(robot_to_ball_distance_x_mm)
+                    self.get_vision_all()
+                    robot_to_ball_distance_y_mm = BEV_FRAME_HEIGHT_MM - self.ball_coordinate_y
+                    self.MOTION.walk_forward(robot_to_ball_distance_y_mm)                    
+                    self.MOTION.touch_ball()
+                    #print("touched")
+                elif self.ball_coordinate_x == 0 and self.ball_coordinate_y == 0:
+                    self.MOTION.stop_motion()
+                    break      
+                print("3")
+            print("2")          
+                
+        #print("NO BALL")
         
     def display_image(self):
         return self.result_image
--- motion_planning_test.py
+++ motion_planning_test.py
@@ -9,14 +9,10 @@ from motion_planning.motion_planning_library import MotionPlanningLibrary
 
 PLANNING = MotionPlanningLibrary()
 
-
-DISTANCE_FROM_THE_EDGE_MAXIMUM_MM = 300
-DISTANCE_FROM_THE_EDGE_MINIMUM_MM = 150
-DISTANCE_FROM_THE_EDGE_DEFAULT_MM = 200
-
 while True:
-    # PLANNING.left_hand_approach()
+    PLANNING.left_hand_approach()
     PLANNING.approach_to_ball()
+    PLANNING.get_vision_all()
     cv2.imshow("frame3", PLANNING.display_image())
     
     if cv2.waitKey(1) & 0xFF == ord('q'):
