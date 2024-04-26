5a9b09595677970da6b5978b7c524b8c8956704f
TaikiTsuno <taikiyoyo@gmail.com>
2024-04-26 13:31:50 +0900

    ファイルの移動

--- for_test/detect_goal_test.py
+++ for_test/detect_goal_test.py
@@ -1,3 +1,5 @@
+#vision_libraryを用いたゴール検出の動作確認用（ゴールラインは白色のため、反射等で検出がうまくいかない際にはhsv閾値の調整を行う）
+
 from vision.vision_library import VisionLibrary
 import cv2
 
@@ -9,6 +11,5 @@ while True:
     frame_mask, frame = VISION.detect_goal()
     cv2.imshow("frame1", frame_mask)
     cv2.imshow("frame2", frame)
-
     if cv2.waitKey(delay) & 0xFF == ord('q'):
         break
