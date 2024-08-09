1985bb92b42cdef40c4ddc4b44d9c19e71b0c90d
TaikiTsuno <taikiyoyo@gmail.com>
2024-08-09 18:40:20 +0900

    0809

--- sample/motion_planning_test.py
+++ sample/motion_planning_test.py
@@ -6,19 +6,19 @@ import sys
 import numpy as np
 import cv2
 
-from motion_planning.motion_planning_library import MotionPlanningLibrary
+from task_execute.task_execute_library import TaskExecuteLibrary
 
-PLANNING = MotionPlanningLibrary()
+TASKEXECUTE = MotionPlanningLibrary()
 
 while True:
-    PLANNING.left_hand_approach()
-    # PLANNING.approach_to_ball()
-    # print(PLANNING.check_know_ball_pos())
-    #PLANNING.get_vision_all()
-    #PLANNING.get_angle_to_goal()
-    # PLANNING.round_corner()
-    # PLANNING.align_with_field_edge()
-    # PLANNING.cross_goal()
-    cv2.imshow("RESULT IMAGE", PLANNING.display_image())
+    TASKEXECUTE.left_hand_approach()
+    # TASKEXECUTE.approach_to_ball()
+    # print(TASKEXECUTE.check_know_ball_pos())
+    #TASKEXECUTE.get_vision_all()
+    #TASKEXECUTE.get_angle_to_goal()
+    # TASKEXECUTE.round_corner()
+    # TASKEXECUTE.align_with_field_edge()
+    # TASKEXECUTE.cross_goal()
+    cv2.imshow("RESULT IMAGE", TASKEXECUTE.display_image())
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
